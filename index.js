97d33092d0cdf71a23ed2a0c0dc8f3ca383e0c0d
Reina <r.clarkemitchell@gmail.com>
2020-12-10 23:03:21 -0500

    js skeleton calculate rate works.

--- index.js
+++ index.js
@@ -5,14 +5,27 @@ const amountTwo = document.querySelector('#amount-two');
 const rate = document.querySelector('#rate');
 const swap = document.querySelector('#swap');
 
+console.log(rate);
+
 function calculate() {
   const currency_one = currencyEleOne.value;
   const currency_two = currencyEleTwo.value;
-  console.log(currency_one);
-  console.log(currency_two);
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+    .then((response) => response.json())
+    .then((response) => {
+      const rateOfSecond = response.rates[currency_two];
+      amountTwo.value = (amountOne.value * rateOfSecond).toFixed(2);
+      innerTextOfConversion(currency_one, currency_two, rateOfSecond);
+    });
+}
+
+function innerTextOfConversion(currency_one, currency_two, rateOfSecond) {
+  rate.innerText = `1 ${currency_one} = ${rateOfSecond} ${currency_two} `;
 }
 
 currencyEleOne.addEventListener('change', calculate);
 amountOne.addEventListener('input', calculate);
 currencyEleTwo.addEventListener('change', calculate);
 amountTwo.addEventListener('input', calculate);
+
+calculate();
